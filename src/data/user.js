a52799dd16889ac95b08fb6a076a05531ca30eb0
arenga <arengapinnatamarzuki@gmail.com>
2024-10-02 23:24:24 +0700

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,81 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "Tharindu N.",
+		email: "arengapinnatamarzuki@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/arenga18",
+		linkedin: "https://www.linkedin.com/in/arengapm",
+		instagram: "https://instagram.com/arenga_pinnata18",
+	},
+
+	homepage: {
+		title: "Full-stack web and Software Engineer.",
+		description:
+			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Arenga PM. I live in Ciledug Tangerang, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Waste Management System",
+			description:
+				"Developed a waste management application to support a community-driven waste bank service in Meruya Selatan. The app allows users to track waste deposits, earn rewards, and contribute to local environmental sustainability efforts.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/php/php.png",
+			linkText: "View Project",
+			link: "https://bangimam.com",
+		},
+
+		{
+			title: "Sales Dashboard",
+			description:
+				"Built a web-based dashboard to analyze New York property sales data for Identified underlying factors and insights driving the trends observed in the data.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://km-feb24-section-jayapura-team-12.vercel.app/",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
